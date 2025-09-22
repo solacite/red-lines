372564a0072661acee1a0a738367a555d5a0b4b0
solacite <julia.nguyen.do@gmail.com>
2025-09-21 23:36:12 -0400

    fix camera, add web export, add subway car w/ new textures

--- scenes/camera_shake.gd
+++ scenes/camera_shake.gd
@@ -4,10 +4,16 @@ extends Node3D
 @export var shake_duration: float = 0.05
 
 var time_since_shake_start = 0.0
+var original_transform: Transform3D
+
+func _ready():
+	original_transform = self.transform
 
 func _process(delta):
 	time_since_shake_start += delta
 	if time_since_shake_start > shake_duration:
 		var shake_offset = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)) * shake_intensity
-		self.transform.origin = shake_offset
+		
+		self.transform.origin = original_transform.origin + shake_offset
+		
 		time_since_shake_start = 0
